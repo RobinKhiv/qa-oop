03056618172ad23277612da0c4b90d5b1d3749e1
Robin Khiv <robinkhiv@gmail.com>
2021-05-19 12:37:36 -0700

    rk:added person class

--- src/com/qa/start/oop/classes/Person.java
+++ src/com/qa/start/oop/classes/Person.java
@@ -1,5 +1,20 @@
 package com.qa.start.oop.classes;
 
 public class Person {
-
+	private String name; 
+	public int age;
+	public String gender;
+	public boolean isBreathing;
+	
+	Person(String name){
+		this(name, 0, "");
+	}
+	Person(String name, int age, String gender){
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+	public String getName() {
+		return this.name;
+	}
 }
