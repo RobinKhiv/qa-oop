e22c7b26d37cdba68469b7bdb2aa3d847443290d
Robin Khiv <robinkhiv@gmail.com>
2021-05-19 12:46:32 -0700

    rk:added method to read in Person class

--- src/com/qa/start/Runner.java
+++ src/com/qa/start/Runner.java
@@ -7,10 +7,17 @@ public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Create an instance of a Person who has a name.
 		Person p1 = new Person("John");
+		//Create an instance of a Book with a title
 		Book b1 = new Book("Introduction To Algorithms");
+
+		//implement a read method in the Person class
+		//use your instance of Person to read the book - so that:
+		//"{person name} reads: {book title here}"
+		p1.reads(b1.getTitle());
 		
-		System.out.println(p1.getName() + " reads: " + b1.getTitle());
+		
 	}
 
 }
