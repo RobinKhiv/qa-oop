23d9ec36284d0d023726065fa039c53367f68c5d
Robin Khiv <robinkhiv@gmail.com>
2021-05-19 12:33:33 -0700

    rk:added book class exercise

--- src/com/qa/start/oop/classes/Book.java
+++ src/com/qa/start/oop/classes/Book.java
@@ -1,5 +1,26 @@
 package com.qa.start.oop.classes;
 
 public class Book {
+	private String title;
+	private String author;
+	private String genre;
+	private String edition;
+	private int pageCount;
+	private boolean isOpen;
 
+	public Book(String title) {
+		this(title, 0, "");
+	}
+	public Book(String title, int pageCount, String edition) {
+		this.title = title;
+		this.pageCount = pageCount;
+		this.edition = edition;
+		this.isOpen = false;
+	}
+	public void open() {
+		this.isOpen = true;
+	}
+	public void close() {
+		this.isOpen = false;
+	}
 }
