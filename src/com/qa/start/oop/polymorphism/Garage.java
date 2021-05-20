552a7bc135f7a764e5c43f9a8273824d20c6fd96
Robin Khiv <robinkhiv@gmail.com>
2021-05-20 13:26:25 -0700

    rk:added garage exercise

--- src/com/qa/start/oop/polymorphism/Garage.java
+++ src/com/qa/start/oop/polymorphism/Garage.java
@@ -1,5 +1,44 @@
 package com.qa.start.oop.polymorphism;
 
-public class Garage {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.qa.start.oop.classes.parent.Vehicle;
+import com.qa.start.oop.classes.parent.child.Car;
+import com.qa.start.oop.classes.parent.child.Motorcycle;
+
+public class Garage {
+	// Using a List implementation, store all your vehicles in a Garage class.
+	List<Vehicle> storage;
+	public Garage() {
+		storage = new ArrayList<Vehicle>();
+	}
+	//Create a method in Garage that iterates through each Vehicle, calculating
+	//	a bill for each type of Vehicle in a different way, depending on the type 
+	//	of vehicle it is.  You can decide how this bill is calculated based on 
+	//	any attributes you see fit. (You do not get extra marks for making the 
+	//	calculation overly complex) 
+	public double getBill() {
+		double total = 0d;
+		
+		for(Vehicle v: storage) {
+			if(v instanceof Car)
+				total+= 500d;
+			else if (v instanceof Motorcycle)
+				total+= 200d;
+			else
+				total+= 100d;
+		}
+		
+		return total;
+	}
+	 
+	//Garage should have methods that add Vehicle, remove Vehicle(s)
+	//	(By ID, By Vehicle Type) fix Vehicle (Calculate bill) and empty the garage
+	public void add(Vehicle v) {
+		storage.add(v);
+	}
+	public void remove(Vehicle v) {
+		storage.remove(v);
+	}
 }
--- src/com/qa/start/oop/polymorphism/Polymorphism.java
+++ src/com/qa/start/oop/polymorphism/Polymorphism.java
@@ -1,9 +1,15 @@
 package com.qa.start.oop.polymorphism;
 
+import com.qa.start.oop.classes.parent.Vehicle;
+import com.qa.start.oop.classes.parent.child.Car;
+import com.qa.start.oop.classes.parent.child.Motorcycle;
+
 public class Polymorphism {
 	public static void start() {
 		//1). Using Vehicle as a base class, create three derived classes (car, motorcycle etc.), 
 		//	each derived class should have its own attributes in addition to the normal Vehicle attributes. 
-		//2). Using a List implementation, store all your vehicles in a Garage class.  
+		Vehicle car1 = new Car("Mazda");
+		Vehicle moto = new Motorcycle("harley");
+		//2). Using a List implementation, store all your vehicles in a Garage class.
 	}
 }
