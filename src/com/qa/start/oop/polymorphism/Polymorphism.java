bc3c629310f66281a166382a159194549a1700ac
Robin Khiv <robinkhiv@gmail.com>
2021-05-20 13:39:07 -0700

    rk:added boat subclass to runner

--- src/com/qa/start/oop/polymorphism/Polymorphism.java
+++ src/com/qa/start/oop/polymorphism/Polymorphism.java
@@ -1,6 +1,7 @@
 package com.qa.start.oop.polymorphism;
 
 import com.qa.start.oop.classes.parent.Vehicle;
+import com.qa.start.oop.classes.parent.child.Boat;
 import com.qa.start.oop.classes.parent.child.Car;
 import com.qa.start.oop.classes.parent.child.Motorcycle;
 
@@ -10,14 +11,20 @@ public class Polymorphism {
 		//	each derived class should have its own attributes in addition to the normal Vehicle attributes. 
 		Vehicle car1 = new Car("Mazda");
 		Vehicle moto = new Motorcycle("harley");
+		Vehicle boat = new Boat("Sea Ray");
 		//2). Using a List implementation, store all your vehicles in a Garage class.
+		
 		Garage g = new Garage();
+		
 		double bill = 0d;
 		g.add(car1);
 		g.add(moto);
+		g.add(boat);
 		bill = g.getBill();
-		
 		System.out.println("Garage bill: $" + bill );
 		
+		g.remove(boat);
+		bill = g.getBill();
+		System.out.println("Garage bill w/o boat: $" + bill);
 	}
 }
