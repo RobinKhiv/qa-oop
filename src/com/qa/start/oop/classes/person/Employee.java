4432753e53084061b9f1ae7df10886b742d3e389
Robin Khiv <robinkhiv@gmail.com>
2021-05-20 08:04:47 -0700

    rk:added constructor and method to employee class

--- src/com/qa/start/oop/classes/person/Employee.java
+++ src/com/qa/start/oop/classes/person/Employee.java
@@ -1,5 +1,26 @@
 package com.qa.start.oop.classes.person;
 
-public class Employee {
+import com.qa.start.oop.classes.Person;
 
+public class Employee extends Person {
+	//a field annualSalary of type double
+	private double annualSalary;
+	//a field weeklySalary of type double
+	private double weeklySalary;
+	//a method calculateWeeklySalary() which makes use of the formula weeklySalary = annualSalary / 52.
+	
+	public Employee() {
+		this("unknown");
+	}
+	public Employee(String name){
+		this(name, 0);
+	}
+	public Employee(String name, double annualSalary) {
+		super(name);
+		this.annualSalary = annualSalary;
+		this.weeklySalary = calculateWeeklySalary();
+	}
+	public double calculateWeeklySalary() {
+		return annualSalary/52;
+	}
 }
